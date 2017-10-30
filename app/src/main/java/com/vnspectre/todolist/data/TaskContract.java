4d8e7159d6853aeaacda794d7e83432970260b25
Chúc Bá Hiếu <Spectre@Chucs-MacBook-Pro.local>
2017-10-30 16:43:34 +0700

    Add URIs to Contract

--- app/src/main/java/com/vnspectre/todolist/data/TaskContract.java
+++ app/src/main/java/com/vnspectre/todolist/data/TaskContract.java
@@ -1,5 +1,6 @@
 package com.vnspectre.todolist.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,7 +9,17 @@ import android.provider.BaseColumns;
 
 public class TaskContract  {
 
+    // The authority, which is how your code knows which Content Provider to access.
+    public static final String AUTHORITY = "com.vnspectre.todolist";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    // This is the path for the "tasks" directory.
+    public static final String PATH_TASKS = "tasks";
+
     public static final class TaskEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_TASKS).build();
 
         public static final String TABLE_NAME = "tasks";
 

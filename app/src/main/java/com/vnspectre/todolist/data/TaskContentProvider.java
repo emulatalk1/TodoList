b53f71d83097757da1c73183b9ee3024e1ca8f25
Chúc Bá Hiếu <Spectre@Chucs-MacBook-Pro.local>
2017-10-31 09:37:15 +0700

    Add a URIMatcher

--- app/src/main/java/com/vnspectre/todolist/data/TaskContentProvider.java
+++ app/src/main/java/com/vnspectre/todolist/data/TaskContentProvider.java
@@ -3,6 +3,7 @@ package com.vnspectre.todolist.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -13,6 +14,27 @@ import android.support.annotation.Nullable;
  */
 
 public class TaskContentProvider extends ContentProvider {
+
+    public static final int TASKS = 100;
+    public static final int TASK_WITH_ID = 101;
+
+    private static final UriMatcher mUriMatcher = buildUriMatcher();
+
+    public static UriMatcher buildUriMatcher() {
+
+        // Initialize a UriMatcher with no matches by passing in NO_MATCH to the constructor
+        UriMatcher uriMatcher= new UriMatcher(UriMatcher.NO_MATCH);
+
+        /*
++          All paths added to the UriMatcher have a corresponding int.
++          For each kind of uri you may want to access, add the corresponding match with addURI.
++          The two calls below add matches for the task directory and a single item by ID.
++         */
+        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS, TASKS);
+        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS + "/#", TASK_WITH_ID);
+
+        return uriMatcher;
+    }
 
     private TaskDbHelper mTaskDbHelper;
 
